f6a444bdecf8b83bcaa9f9824b058392cb9b8cd9
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-07-29 12:39:49 +0900

    printer rework

--- homework/day02/PrintFormat.java
+++ homework/day02/PrintFormat.java
@@ -28,6 +28,7 @@ public class PrintFormat {
 
         System.out.println("환자의 ID를 입력하세요.");
         int id = scanner.nextInt();
+
         // nextLine은 엔터를 치는 순간 입력이 되었다고 인식하기 때문에, nextInt와 같은 메소드를
         // 사용할 때, 입력이 꼬이지 않게 nextLine 메소드를 별도록 사용하였다.
         // 그냥 한칸 띄고 환자의 병명을 입력할 순 있지만, 사용자들은 보통 다음 문장을 칠때
@@ -36,31 +37,16 @@ public class PrintFormat {
 
         System.out.println("환자의 병명을 입력하세요.(5자 이하로 입력해주세요.)");
         String s1 = scanner.nextLine();
-        String s2 = null;
-        if(s1.length() > 5){
-            System.out.println("병명의 입력범위를 초과하였습니다. 다시 실행해서 입력해주세요.");
+        System.out.println("환자의 비고 사항을 입력하세요.(10자 이하로 입력해주세요.)");
+        String s2 = scanner.nextLine();
+
+        if(s1.length() > 5 || s2.length() > 10 || id >= 1000000){
+            System.out.println("입력범위를 초과하셨습니다. 다시 실행해서 입력 해주세요.");
             System.exit(0);
         }else{
-            System.out.println("환자의 비고 사항을 입력하세요.(10자 이하로 입력해주세요.)");
-            s2 = scanner.nextLine();
-            if(s2.length() > 10){
-                System.out.println("비고사항의 입력범위를 초과하였습니다. 다시 실행해서 입력해주세요.");
-                System.exit(0);
-            }else{
-                System.out.println("*----------*--------*---------------*");
-
-                System.out.print( id >= 1000000 ? "| ID 입력범위를 초과 하셨습니다." :
-                        id >= 100000 ? "| ID" + id :
-                        id >= 10000 ? "| ID0" + id :
-                        id >= 1000 ? "| ID00" + id :
-                        id >= 100 ? "| ID000" + id :
-                        id >= 10 ? "| ID0000" + id :
-                        id >= 1 ? "| ID00000" + id :
-                                "| ID000000");
-
-                System.out.printf(id >= 1000000 ?  "\n" : " | %-5s | %-10s |\n", s1, s2);
-                System.out.println("*----------*--------*---------------*");
-            }
+            System.out.println("*----------*--------*---------------*");
+            System.out.printf("| id%06d | %-5s | %-10s |\n",id, s1, s2);
+            System.out.println("*----------*--------*---------------*");
         }
     }
 }
