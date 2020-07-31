e33ea4deae3d95df1095c3b074d1e0ad6e8d2ffd
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-07-31 18:58:28 +0900

    MatMul underworld version

---
+++ homework/day04/MatMul_underworld_ver.java
@@ -0,0 +1,31 @@
+package com.company;
+
+/**
+ * 행렬의 곱 계산하기
+ *
+ * 두 행렬의 곱을 구하는 프로그램을 작성하시오.
+ * 행렬의 곱을 계산한 후에 행렬 형태로 출력하시오.
+ *
+ *
+ * 인자
+ * matA: N x M 행렬
+ * matB: M x K 행렬
+ */
+
+public class MatMul_underworld_ver {
+    public static void main(String[] args) {
+        int [][] matA = {{1, 2, 3} ,{4, 5, 2}};
+        int [][] matB = {{5, 2}, {6, 2}, {1, 0}};
+        int [][] matC = new int[matA.length][matB[0].length];
+
+        for (int i = 0; i < matC.length; i++){
+            for (int j = 0; j < matC[i].length; j++) {
+                for (int k = 0; k < matA[i].length; k++){
+                    matC[i][j] += matA[i][k] * matB[k][j];
+                }
+                System.out.print(matC[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
